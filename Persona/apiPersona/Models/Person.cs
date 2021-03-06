471e0f1f8849a0ecf6cce039f7a99c27b257fc1b
Marcelo Ortiz Peredo <a2018119133@estudiantes.upsa.edu.bo>
2021-03-06 10:15:07 -0400

    Modelo Person creado

---
+++ Persona/apiPersona/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace apiPersona.Models
+{
+    public enum SexType         //Lista para suponer que "0" es Femenino
+    {
+        Femenino=0,
+        Masculino=1,
+    }
+    public class Person
+    {
+        [Key]
+        public int PersonId { get; set; }
+        [Required]
+        [StringLength(250,ErrorMessage ="Ingresar Nombre entre a 5 a 250 Caracteres",MinimumLength =5)]
+        public string Name { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)]
+        public DateTime BirthDate { get; set; }
+        public SexType Sex { get; set; }
+    }
+}
